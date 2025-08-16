c164253858b1732daaceaaa3972ba52de232d6fe
Gregory Wagonblast <ghw3@pitt.edu>
2025-08-16 12:57:39 -0400

    Add platform character and initial logic without physics.

--- scenes/scripts/platty.gd
+++ scenes/scripts/platty.gd
@@ -1,18 +1,24 @@
-extends Sprite2D
+extends CharacterBody2D
 
 var pos: Vector2 = Vector2.ZERO
 const speed: int = 500
+var screen_size
+var x_pos
+var y_pos
+var platty_size
  
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pos = Vector2(600, 650)
+	screen_size = get_viewport_rect().size
+	x_pos = get_viewport_rect().size.x
+	y_pos = get_viewport_rect().size.y
+	pos = Vector2(x_pos/2, y_pos)
 	position = pos
-#	Input.is_action_just_pressed("left")
-#	Input.is_action_just_pressed("right")
-
+	platty_size = $Platty_Sprite2D.texture.get_size()/2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# User input.
 	var direction = Input.get_vector("left", "right", "up", "down")
 	position += direction * speed * delta
+	position = position.clamp(Vector2.ZERO+platty_size, screen_size-platty_size)
