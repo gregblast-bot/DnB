b32aa8457104149623b3a507bb0c582659c30a28
Gregory Wagonblast <ghw3@pitt.edu>
2025-08-16 14:05:59 -0400

    La esperma es en mis pelotas.

---
+++ scenes/scripts/ball.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+var velocity: Vector2 = Vector2.ZERO
+var direction = Vector2(0, 0)
+var screen_size
+var x_pos
+var y_pos
+ 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	x_pos = get_viewport_rect().size.x
+	y_pos = get_viewport_rect().size.y - 100
+	position = Vector2(x_pos/2, y_pos)
+
+func _physics_process(delta):
+	# Set direction based on whether lauch is initiated
+	#if Input.get("launch")
+	direction = Vector2(0, 100)
+	apply_force(direction)
